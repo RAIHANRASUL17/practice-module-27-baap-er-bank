dff8858fc1f9f7b8da355f941e25422be8d7d95f
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-16 18:21:26 +0800

    the validation part with all to see index nineHtml and withdraw nine js

--- js/withdraw-9.js
+++ js/withdraw-9.js
@@ -3,17 +3,41 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount= parseFloat(withdrawField.value);
  //    clear withdrawField
  withdrawField.value= '';
-    
+    /*------------------Validation Part-2 Start----------------------*/
+    if(isNaN(newWithdrawAmount)){
+      alert('please put number value');
+      return;
+    }
+    /*------------------Validation Part-2 End----------------------*/
+ 
  
    const withdrawElement= document.getElementById('withdrw-total');
    const previousWithdrawAmount= parseFloat(withdrawElement.innerText);
+
  //   calculation newWithdrawAmount + previousWithdrawAmount then set up 
+/* for validation jus put under the if conditon from here
  const currentWithdrawTotal= newWithdrawAmount + previousWithdrawAmount;
  withdrawElement.innerText= currentWithdrawTotal;
+ 
+**/  
+
  
  /*witdraw With Balance Section*/
  const balanceTotalElement= document.getElementById('balance-total');
  const previouBalanceTotal= parseFloat(balanceTotalElement.innerText);
+
+/*------------------Validation Part-1 Start----------------------*/
+if(newWithdrawAmount > previouBalanceTotal){
+  alert('Don t have have enough money in your Balance');
+  return;
+}
+
+const currentWithdrawTotal= newWithdrawAmount + previousWithdrawAmount;
+withdrawElement.innerText= currentWithdrawTotal;
+
+/*------------------Validation Part-1 End  ----------------------*/ 
+
+
  // calculation previousBalanceTotal - newWithdrawAmount then set up
  const currentBalanceTotal= previouBalanceTotal - newWithdrawAmount;
  // set up
