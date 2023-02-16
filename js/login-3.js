bcc9d991338dbfa606a0fefba88a8d62a8582527
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-16 14:56:36 +0800

    fourth commit set index clearly

--- js/login-3.js
+++ js/login-3.js
@@ -10,8 +10,10 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // verify
     if(email === 'smaraihanrasul17cqut@gmail.com' && password === '09877890'){
         console.log('Valid User');
+        window.location.href= 'bank-4.html'
     }
     else{
         console.log('Invalid User');
+        alert('Hei!!! You are not a correct person, go away police is coming')
     }
 })
